e940c26afbf748c3155bf4f48b87f4be613bc4a1
Emmanuel <emmanueltopea@gmail.com>
2020-06-13 00:53:47 +0100

    initiating test for TDD

--- tests.py
+++ tests.py
@@ -1,1 +1,13 @@
-from unittest import TestCase
+import unittest
+from selenium import webdriver
+from models import User, Channel, Msg
+
+driver = webdriver.Chrome()
+
+class AuthenticationTests(unittest.TestCase):
+	""" Tests for user authentication"""
+	def test_register_user(self):
+		pass
+
+if __name__ == "__main__":
+	unittest.main()
