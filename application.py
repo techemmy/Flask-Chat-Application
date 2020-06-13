a784fdc961a1b005a768a114c04fbc22cab03996
Emmanuel <emmanueltopea@gmail.com>
2020-06-13 23:07:45 +0100

    changed configuration settings

--- application.py
+++ application.py
@@ -6,123 +6,138 @@ from forms import SignUpForm
 from passlib.hash import sha256_crypt
 from functools import wraps
 
+def create_app(test_config=None):
+    app = Flask(__name__)
 
-app = Flask(__name__)
-SECRET_KEY = os.urandom(32)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = SECRET_KEY
-db.init_app(app)
-Session(app)
+    SECRET_KEY = os.urandom(32)
 
-def main():
-    db.create_all()
-    
+    app.config.from_mapping(
+        SESSION_PERMANENT = False,
+        SESSION_TYPE = "filesystem",
+        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL'),
+        SQLALCHEMY_TRACK_MODIFICATIONS = False,
+        SECRET_KEY = SECRET_KEY,
+    )
 
-def login_required(f):
-	@wraps(f)
-	def wrap(*args, **kwargs):
-		if session.get('logged_in'):
-			return f(*args, **kwargs)
-		else:
-			flash("You need to login first")
-			return redirect(url_for('login'))
-	return wrap
-
-def logout_required(f):
-    @wraps(f)
-    def wrapped_view(*args, **kwargs):
-        if not session.get('logged_in'):
-            return f(*args, **kwargs)
-        else:
-            return redirect(url_for('chat'))
-    return wrapped_view
-
-
-@app.route('/<path:urlpath>/')
-@app.route('/', methods=['POST', 'GET'])
-def index(urlpath='/'):
-    if session.get('logged_in'):
-        return redirect(url_for('chat'))
+    if test_config is None:
+        # load the app config from file if not testing
+        app.config.from_pyfile("application.py")
     else:
-        form = SignUpForm()
-        return render_template('main/home.html', form=form)
+        # if testing load app config on tests config
+        app.config.from_mapping(test_config)
 
+    def main():
+        db.create_all()
+        
 
-@app.route('/sign-up/', methods=['POST', 'GET'])
-@logout_required
-def sign_up():
-    form = SignUpForm()
-    print("validating...")
-    if form.validate_on_submit():
-        try:
-            hashed_password = sha256_crypt.hash(form.password.data)
-            firstname = form.firstname.data
-            lastname = form.lastname.data
-            username = form.username.data
-            email = form.email.data
-            password = hashed_password
-            terms = form.tos.data
-            l = User.query.filter_by(username=username).first()
-            u = User.query.filter_by(email=email).first()
-            if l is None:
-                if u is None:
-                    User.add_user(firstname=firstname, lastname=lastname, username=username,
-                            email=email, password=password, terms=terms)
-                    print("validated...")
-                    flash("You have been signed up successfully! Now login your details")
-                    return redirect(url_for('login'))
-                else:
-                    flash("Email taken already")
+    def login_required(f):
+    	@wraps(f)
+    	def wrap(*args, **kwargs):
+    		if session.get('logged_in'):
+    			return f(*args, **kwargs)
+    		else:
+    			flash("You need to login first")
+    			return redirect(url_for('login'))
+    	return wrap
+
+    def logout_required(f):
+        @wraps(f)
+        def wrapped_view(*args, **kwargs):
+            if not session.get('logged_in'):
+                return f(*args, **kwargs)
             else:
-                flash("Username already exists.")
-        except Exception as e:
-            flash(e)
-            return redirect(url_for('index'))
-    return render_template('main/home.html', form=form)
-    
-
-@app.route('/login/', methods=['POST', 'GET'])
-@logout_required
-def login():
-    try:
-        if request.method == 'POST':
-            username = request.form.get('username')
-            password = request.form.get('password')
-            u = User.query.filter_by(username=username).first()
-            try:    
-                v = sha256_crypt.verify(password, u.password)
-            except:
-                pass
-            if u and v:
-                print("Validated!")
-                session["logged_in"] = True
-                session["username"] = u.username
-                print(session["logged_in"], session["username"])
-                flash(f"You are now logged in!")
                 return redirect(url_for('chat'))
-            else:
-                flash("Invalid Login Details!")
+        return wrapped_view
+
+
+    @app.route('/<path:urlpath>/')
+    @app.route('/', methods=['POST', 'GET'])
+    def index(urlpath='/'):
+        if session.get('logged_in'):
+            return redirect(url_for('chat'))
         else:
-            pass
-    except Exception as e:
-        print(e)
-
-    return render_template('main/login.html')
-
-@app.route("/logout/")
-@login_required
-def logout():
-    session.clear()
-    return redirect(url_for('index'))
+            form = SignUpForm()
+            return render_template('main/home.html', form=form)
 
 
-@app.route('/chat/')
-@login_required
-def chat():
-    return render_template('main/chat.html')
+    @app.route('/sign-up/', methods=['POST', 'GET'])
+    @logout_required
+    def sign_up():
+        form = SignUpForm()
+        print("validating...")
+        if form.validate_on_submit():
+            try:
+                hashed_password = sha256_crypt.hash(form.password.data)
+                firstname = form.firstname.data
+                lastname = form.lastname.data
+                username = form.username.data
+                email = form.email.data
+                password = hashed_password
+                terms = form.tos.data
+                l = User.query.filter_by(username=username).first()
+                u = User.query.filter_by(email=email).first()
+                if l is None:
+                    if u is None:
+                        User.add_user(firstname=firstname, lastname=lastname, username=username,
+                                email=email, password=password, terms=terms)
+                        print("validated...")
+                        flash("You have been signed up successfully! Now login your details")
+                        return redirect(url_for('login'))
+                    else:
+                        flash("Email taken already")
+                else:
+                    flash("Username already exists.")
+            except Exception as e:
+                flash(e)
+                return redirect(url_for('index'))
+        return render_template('main/home.html', form=form)
+        
+
+    @app.route('/login/', methods=['POST', 'GET'])
+    @logout_required
+    def login():
+        try:
+            if request.method == 'POST':
+                username = request.form.get('username')
+                password = request.form.get('password')
+                u = User.query.filter_by(username=username).first()
+                try:    
+                    v = sha256_crypt.verify(password, u.password)
+                except:
+                    pass
+                if u and v:
+                    print("Validated!")
+                    session["logged_in"] = True
+                    session["username"] = u.username
+                    print(session["logged_in"], session["username"])
+                    flash(f"You are now logged in!")
+                    return redirect(url_for('chat'))
+                else:
+                    flash("Invalid Login Details!")
+            else:
+                pass
+        except Exception as e:
+            print(e)
+
+        return render_template('main/login.html')
+
+    @app.route("/logout/")
+    @login_required
+    def logout():
+        session.clear()
+        return redirect(url_for('index'))
+
+
+    @app.route('/chat/')
+    @login_required
+    def chat():
+        return render_template('main/chat.html')
+
+
+    db.init_app(app)
+    Session(app)
+    
+    return app
 
 
 if __name__ == "__main__":
