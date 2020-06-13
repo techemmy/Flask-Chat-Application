d35c4695f4eb18aa349b55b64621140dff2dcd8a
Emmanuel <emmanueltopea@gmail.com>
2020-06-13 23:11:09 +0100

    TDD cover up

--- tests.py
+++ tests.py
@@ -1,13 +1,25 @@
 import unittest
 from selenium import webdriver
 from models import User, Channel, Message, messages_relations, members
+from application import app
+from flask import session
 
-# driver = webdriver.Chrome()
+driver = webdriver.Chrome("C:\Program Files\chromedriver.exe")
 
-class AuthenticationTests(unittest.TestCase):
-	""" Tests for user authentication"""
-	def test_register_user(self):
-		pass
+class AuthenticationViewTests(unittest.TestCase):
+	""" Tests for views of user authentication"""
+	def test_user_not_connected_homepage(self):
+		driver.get("localhost:5000")
+		signup_btn = driver.find_element_by_id("signup_btn")
+		login_btn = driver.find_element_by_id("login_btn")
+		self.assertEqual(driver.title, "Hook - Connect")
+		self.assertEqual(signup_btn.text, "Sign Up")
+		self.assertEqual(login_btn.text, "Log In")
+
+	def test_user_login_redirect(self):
+		driver.get("localhost:5000/chat")
+		message = driver.find_element_by_id("alert-message")
+		self.assertEqual(message.text, "You need to login first\n×")
 
 if __name__ == "__main__":
 	unittest.main()
