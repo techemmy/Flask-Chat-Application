088b15c94ddc150510c1443bf4c51058df7854f6
Emmanuel <emmanueltopea@gmail.com>
2020-06-15 02:51:00 +0100

    modified

--- tests.py
+++ tests.py
@@ -1,13 +1,31 @@
 import unittest
+import os
+from flask_testing import TestCase
+from application import create_app, User
+from models import db
 from selenium import webdriver
-from models import User, Channel, Message, messages_relations, members
-from application import create_app
-from flask import session
 
-driver = webdriver.Chrome("C:\Program Files\chromedriver.exe")
 
-class AuthenticationViewTests(unittest.TestCase):
+driver = webdriver.Chrome()
+
+class AuthenticationTests(TestCase):
 	""" Tests for views of user authentication"""
+	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+	TESTING = True
+			
+	def create_app(test_config):
+		test_config = {
+			'ENV': 'test'
+		}
+		return create_app(test_config)
+
+	def setUp(self):
+		db.create_all()
+
+	def tearDown(self):
+		db.session.remove()
+		db.drop_all()
+
 	def test_user_not_connected_homepage(self):
 		driver.get("localhost:5000")
 		signup_btn = driver.find_element_by_id("signup_btn")
@@ -20,6 +38,12 @@ class AuthenticationViewTests(unittest.TestCase):
 		driver.get("localhost:5000/chat")
 		message = driver.find_element_by_id("alert-message")
 		self.assertEqual(message.text, "You need to login first\n×")
+
+	def test_registered_user(self):
+		# user = User.add_user("FirstName", "LastName", "User", "Email@gmail.com", "password123", True)
+		# db.session.add(user)
+		# assert user in db.session
+		pass
 
 if __name__ == "__main__":
 	unittest.main()
