1cc0693b5ee4c7f9358e2703312c6698e740d711
Emmanuel <emmanueltopea@gmail.com>
2020-06-18 00:47:11 +0100

    modify to meet flake8 requirements

--- forms.py
+++ forms.py
@@ -2,12 +2,22 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, EqualTo, Length
 
+
 class SignUpForm(FlaskForm):
-    firstname =  StringField('Firstname:',  validators=[DataRequired(), Length(min=2, max=20)])
-    lastname =  StringField('Lastname:',  validators=[DataRequired(), Length(min=2, max=20)])
-    username =  StringField('Username:',  validators=[DataRequired(), Length(min=2, max=10)])
-    email = StringField('Email:',  validators=[DataRequired(), Length(min=2, max=30)])
-    password = PasswordField('Password:', validators=[DataRequired(), Length(min=5, max=25),
-                                            EqualTo('confirm', message="Passwords must match.")])
+    firstname = StringField('Firstname:', validators=[DataRequired(),
+                                                      Length(min=2, max=20)])
+    lastname = StringField('Lastname:', validators=[DataRequired(),
+                                                    Length(min=2, max=20)])
+    username = StringField('Username:', validators=[DataRequired(),
+                                                    Length(min=2, max=10)])
+    email = StringField('Email:', validators=[DataRequired(),
+                                              Length(min=2, max=30)])
+    password = PasswordField('Password:', validators=[DataRequired(),
+                                                      Length(min=5, max=25),
+                                                      EqualTo('confirm',
+                                                      message="Passwords \
+                                                      must match.")]
+                             )
     confirm = PasswordField('Repeat Password:')
-    tos = BooleanField('I accept the terms and conditions that apply to this application.', validators=[DataRequired()])
+    tos = BooleanField('I accept the terms and conditions that apply to this \
+                        application.', validators=[DataRequired()])
