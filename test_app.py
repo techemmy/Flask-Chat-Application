4304c9f96e5f07455a53baba2fb5be4e41ba5d2d
Emmanuel <emmanueltopea@gmail.com>
2020-06-21 23:25:23 +0100

    add test logout required,improve test standard

--- test_app.py
+++ test_app.py
@@ -21,21 +21,26 @@ class AuthenticationTests(unittest.TestCase):
         """ Setup a blank test db before each test """
         global app
         basedir = os.path.abspath(os.path.dirname(__file__))
-        hashed_pass = "password123"
+        hashed_pass = "$5$rounds=535000$OAFoz.l/V18RuDF6$/LxX2b865VrNPebfihaw5ZCvjzANjH7FrnFvqsPxBt2"
+
         app = create_app({
                          'TESTING': True,
                          'ENV': 'test',
                          'SQLALCHEMY_DATABASE_URI': 'sqlite:///' +
                          os.path.join(basedir, TEST_DB),
                          'USERNAME': 'Test',
-                         'PASSWORD': hashed_pass,
+                         'PASSWORD': 'password123',
                          })
         self.app = app.test_client()
         app.app_context().push()
         db.create_all()
+        user = User(firstname="Test", lastname="Lase", username="Test",
+                    email="Email@gmail.com", password=hashed_pass, terms=True)
+        user.save()
 
     def tearDown(self):
         db.session.remove()
+        db.drop_all()
 
     def test_homepage(self):
         """ test user not connected page """
@@ -52,10 +57,10 @@ class AuthenticationTests(unittest.TestCase):
     def login(self, username, password):
         """ login helper function """
         return self.app.post('/login/',
-                                 data={'username': username,
-                                       'password': password},
-                                 follow_redirects=True,
-                                 )
+                             data={'username': username,
+                                   'password': password},
+                             follow_redirects=True,
+                             )
 
     def logout(self):
         """ logout helper function """
@@ -70,15 +75,22 @@ class AuthenticationTests(unittest.TestCase):
         self.assertIn(b'You logged out successfully!', request.data)
 
     def test_invalid_login(self):
-    	""" test invalid login using helper function """
-    	request = self.login(app.config['USERNAME'] + 'x',
-                             app.config['PASSWORD'] + 'X')
-    	self.assertIn(b'Check your credentials and try again!', request.data)
+        """ test invalid login using helper function """
+        request = self.login(app.config['USERNAME'] + 'Y',
+                             app.config['PASSWORD'] + 'Y')
+        self.assertIn(b'Check your credentials and try again!', request.data)
 
     def test_login_required_wrapper(self):
-    	""" test if the login required wrapper works """
-    	request = self.logout()
-    	self.assertIn(b'You need to login first', request.data)
+        """ test if the login required wrapper works """
+        request = self.logout()
+        self.assertIn(b'You need to login first', request.data)
+
+    def test_logout_required_wrapper(self):
+        request = self.login(app.config['USERNAME'],
+                             app.config['PASSWORD'])
+        request = self.login(app.config['USERNAME'],
+                             app.config['PASSWORD'])
+        self.assertIn(b'You are need to logout first.', request.data)
 
 
 if __name__ == '__main__':
