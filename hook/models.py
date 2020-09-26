2775ba6376839188aeecfb3ba439180475925477
Emmanuel <emmanueltopea@gmail.com>
2020-09-26 22:43:48 +0100

    change db relationship status

--- hook/models.py
+++ hook/models.py
@@ -1,22 +1,9 @@
-#! usr/env/bin python
+#!usr/bin/env python
 from flask_sqlalchemy import SQLAlchemy
-import datetime
+import time
+
 
 db = SQLAlchemy()
-
-members = db.Table('members',
-                   db.Column('user_id', db.Integer,
-                             db.ForeignKey('users.id'), primary_key=True),
-                   db.Column('channel_id', db.Integer,
-                             db.ForeignKey('channels.id'), primary_key=True)
-                   )
-
-messages_relations = db.Table('messages_relations',
-                              db.Column('message_id', db.Integer,
-                                        db.ForeignKey('messages.id')),
-                              db.Column('channel_id', db.Integer,
-                                        db.ForeignKey('channels.id')),
-                              )
 
 
 class User(db.Model):
@@ -24,11 +11,11 @@ class User(db.Model):
     Users Class for creating the 'users' table in the database
     and for managing user's information
     """
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(20), nullable=False)
     lastname = db.Column(db.String(20), nullable=False)
-    username = db.Column(db.String(10), nullable=False, unique=True)
+    username = db.Column(db.String(15), nullable=False, unique=True)
     email = db.Column(db.String(40), nullable=False)
     password = db.Column(db.Text, nullable=False)
     terms = db.Column(db.Boolean, unique=False, default=False)
@@ -50,43 +37,39 @@ class Channel(db.Model):
     Channel Model for creating the 'channels' table and managing channel
     related objects
     """
-    __tablename__ = 'channels'
+    __tablename__ = 'channel'
     id = db.Column(db.Integer, primary_key=True)
     channel_name = db.Column(db.String(20), nullable=False)
-    member = db.relationship('User', secondary=members, lazy='subquery',
-                             backref=db.backref('members', lazy=True))
+    messages = db.relationship('Message', lazy='select', backref=db.backref('messages', lazy='joined'))
 
     def save(self):
         """ saves a channel and its data """
         db.session.add(self)
         db.session.commit()
 
+    def __str__(self):
+        return f"Channel '{self.channel_name}'"
 
 class Message(db.Model):
     """Message table in the database"""
-    __tablename__ = 'messages'
+    __tablename__ = 'message'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(10), nullable=False)
-    timestamp = db.Column(db.String(10), nullable=False)
+    name = db.Column(db.String(15), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
     message = db.Column(db.Text, nullable=False)
-    channel = db.relationship('Channel', secondary=messages_relations,
-                              lazy='subquery',
-                              backref=db.backref('messages_relations',
-                                                 lazy=True)
-                              )
+    channel_id = db.Column(db.Integer, db.ForeignKey('channel.id'), nullable=True)
+
+    __mapper_args__ = {
+                    'version_id_col': timestamp,
+                    'version_id_generator': lambda v: time.strftime('%H:%M')
+    }
+
+    def __init__(self, name, message):
+        self.name = name
+        self.message = message
 
     def save(self):
         """ saves messages """
         db.session.add(self)
         db.session.commit()
 
-
-class MessageCreator:
-    """
-    Message function for adding messages to the Channel Class or channels table
-    """
-    def __init__(self, name, message):
-        """ configure message's arguments """
-        self.name = name
-        self.timestamp = datetime.datetime.now()
-        self.message = message
