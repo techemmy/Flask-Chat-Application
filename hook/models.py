2ae7fafd10b289d4b594f3a62893edc5b66e8f58
Emmanuel <emmanueltopea@gmail.com>
2020-10-01 00:47:56 +0100

    fix flake8 issues

--- hook/models.py
+++ hook/models.py
@@ -1,6 +1,7 @@
 #!usr/bin/env python
 from flask_sqlalchemy import SQLAlchemy
-import time
+# import time
+import datetime
 
 
 db = SQLAlchemy()
@@ -19,12 +20,14 @@ class User(db.Model):
     email = db.Column(db.String(40), nullable=False)
     password = db.Column(db.Text, nullable=False)
     terms = db.Column(db.Boolean, unique=False, default=False)
+    message = db.relationship('Message', lazy='subquery', backref='user',
+                              order_by='Message.id')
 
     def __repr__(self):
         """
         Returns the user's username when the __str__ function is called.
         """
-        return f"Username :{self.username}"
+        return f"Username:{self.username}"
 
     def save(self):
         """ saves user's data """
@@ -40,36 +43,40 @@ class Channel(db.Model):
     __tablename__ = 'channel'
     id = db.Column(db.Integer, primary_key=True)
     channel_name = db.Column(db.String(20), nullable=False)
-    messages = db.relationship('Message', lazy='select', backref=db.backref('messages', lazy='joined'))
+    messages = db.relationship('Message', lazy='dynamic', backref='channel',
+                               order_by='Message.id')
+
+    def __init__(self, name):
+        self.channel_name = name
 
     def save(self):
         """ saves a channel and its data """
         db.session.add(self)
         db.session.commit()
 
-    def __str__(self):
-        return f"Channel '{self.channel_name}'"
+    def __repr__(self):
+        return f"Channel:'{self.channel_name}'"
+
 
 class Message(db.Model):
     """Message table in the database"""
     __tablename__ = 'message'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(15), nullable=False)
-    timestamp = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     message = db.Column(db.Text, nullable=False)
-    channel_id = db.Column(db.Integer, db.ForeignKey('channel.id'), nullable=True)
+    channel_id = db.Column(db.Integer, db.ForeignKey('channel.id'),
+                           nullable=False)
 
-    __mapper_args__ = {
-                    'version_id_col': timestamp,
-                    'version_id_generator': lambda v: time.strftime('%H:%M')
-    }
+    # __mapper_args__ = {
+    #                 'version_id_col': timestamp,
+    #                 'version_id_generator': lambda v: time.strftime('%H:%M')
+    # }
 
-    def __init__(self, name, message):
-        self.name = name
+    def __init__(self, message):
         self.message = message
 
     def save(self):
         """ saves messages """
         db.session.add(self)
         db.session.commit()
-
