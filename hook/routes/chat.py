99122bddff8a6478c22bfc8868f8c9cf425a2dd4
Emmanuel <emmanueltopea@gmail.com>
2020-10-02 17:00:24 +0100

    add new channel to db and get channel details

--- hook/routes/chat.py
+++ hook/routes/chat.py
@@ -1,21 +1,76 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, request, jsonify
 from hook.routes.auth import login_required
-# from flask_socketio import emit, join_room, leave_room
+from flask_socketio import emit, join_room
 from .. import socketio
+from ..models import Channel, User
 
 chat = Blueprint('chat', __name__)
+
 
 @chat.route('/')
 @login_required
 def index():
     """ chat page if user is logged in """
-    return render_template('main/chat.html')
+    context = {
+    	'channels': Channel.query.all()
+    }
+    return render_template('main/chat.html', context=context)
 
+
+@chat.route('/add-channel', methods=['POST'])
+def add_new_channel():
+    channel_name = request.form.get('channel_name')
+    channel_name = channel_name.strip()
+    if channel_name:
+	    if not Channel.query.filter_by(channel_name=channel_name).all():
+		    new_channel = Channel(channel_name)
+		    new_channel.save()
+
+		    return jsonify({'channel_name': new_channel.channel_name, 'success': True,
+		                    'error': 'null'})
+	    else:
+	    	return jsonify({'success': False, 'error': 'Channel already exists!'})
+    else:
+        return jsonify({'success': False, 'error': 'Invalid input.'})
+
+
+# ============== SOCKETS CODES =================
 
 @socketio.on('connected', namespace='/chat')
 def connected(data):
-	print("""
-		connecting server-side...
-		""")
-	
+	""" confirm connection of sockets """
 	print(data['data'])
+
+
+@socketio.on('getChannelDetails', namespace='/chat')
+def get_channel_details(data):
+	""" Check for existence of channels and get
+	    the details of the channels including the
+	    - channel's name
+	    - channel's messages   """
+	channel_name = data['name'][1:]  # excluding the '#' symbol
+	channel_id = data['id']
+
+	current_user = session.get('user').username # check for alignment of message in script
+	# confirm existence of channel
+	channel = Channel.query.get(channel_id)
+	# on existence get channel messages
+	if channel.channel_name == channel_name:
+		message_objects = channel.messages.all()
+		messages = []
+		for message in message_objects:
+			user = User.query.get_or_404(message.user_id,
+				                         description="User not found.")
+			time = message.timestamp
+			timestamp = ' ' + str(time.date()) + ' | ' + str(time.strftime('%H:%M'))
+			messages.append([user.username, timestamp,
+			                 message.message])
+	else:
+		emit('ChannelDoesNotExist', {'error': 'Channel does not exists'},
+			 broadcast=False)
+	# enter channel as a room
+	room = join_room(channel_name)
+	join_room(channel_name)
+	# return details to script
+	emit('channelMessagesDelivered', {"messages": messages,
+	     "user": current_user}, room=room)
--- hook/static/js/chat-script.js
+++ hook/static/js/chat-script.js
@@ -9,4 +9,133 @@ document.addEventListener('DOMContentLoaded', () => {
 		console.log("Connecting script...");
 		socket.emit('connected', {'data': 'I\'m connected!'});
 	});
+	document.querySelectorAll('.channel').forEach((channel) => {
+		channel.onclick = () => {
+			// Get channel name & id
+			const name = channel.innerHTML;
+			const id = channel.dataset.get;
+			localStorage.setItem('activeChannel', name);
+			localStorage.setItem('id', id)
+			socket.emit('getChannelDetails', {'name': name, 'id': id});
+		};
+	});
+
+	socket.on('channelMessagesDelivered', (data) => {
+		broadcastMessage(data);
+	});
+
+	socket.on('ChannelDoesNotExist', (data) => {
+		alert(data.error)
+	});
+
+
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const addNew = document.querySelectorAll('.add-new').forEach((addNew) => {
+    	addNew.onclick = () => {
+    		document.querySelector('.modal-dialog .modal-title').innerHTML = `Add New ${addNew.dataset.type}`;
+    		$('#add-temp').modal('show');
+    	};
+    });
+
+ //    window.onload = () => {
+	// 	let channelName = localStorage.getItem('activeChannel');
+	// 	let channelId = localStorage.getItem('id')
+	// 	console.log(channelName);
+	// 	socket.emit('getChannelDetails', {'name': name, 'id': id});
+	// }
+
+	document.querySelector('#add-new-form').onsubmit = addNewChannel;
+});
+
+
+// =================== FUNCTIONS =============================== //
+
+
+function addNewChannel(){
+	// initialize AJAX request
+	const request = new XMLHttpRequest();
+	const channelName = document.querySelector('#add-new-field').value;
+	$('#add-temp').modal('hide');
+	document.querySelector('#add-new-field').value = '';
+	request.open('POST', '/chat/add-channel');
+
+	// callback function on completion of request
+	request.onload = () => {
+
+		// extract json data from response
+		const data = JSON.parse(request.responseText);
+
+		// add channel
+		if (data.success){
+			const channel_name = `${data.channel_name}`;
+			console.log(channel_name);
+		} else {
+			alert(data.error);
+		}
+	}
+
+	// add data to send with request
+	const data = new FormData();
+	data.append('channel_name', channelName)
+
+	// send request
+	request.send(data);
+	return false;
+};
+
+
+function broadcastMessage(data) {
+	 document.querySelector('.msg-gutter').innerHTML = '';
+	 document.querySelector('.active-channel').innerHTML = localStorage.getItem('activeChannel');
+	// loop to add message to the message gutter
+	for(i=0; i < data.messages.length; i++){
+		message = data.messages[i]
+
+		let msgContainer = document.createElement('div');
+		msgContainer.className = 'msg-container';
+  		if (data.user === message[0]){
+  			msgContainer.style.justifyContent = 'flex-end';
+  		};
+
+  		let msgInfo = document.createElement('div');
+  		msgInfo.className = 'msg-info';
+
+  		let msgPic = document.createElement('div');
+  		msgPic.className = 'msg-pic';
+
+  		let msgImg = document.createElement('img');
+  		msgImg.src = '../static/assets/msg-img.png';
+  		msgImg.alt = 'user-image';
+  		msgImg.className = 'msg-img';
+
+  		let msgCont = document.createElement('div');
+  		msgCont.className = 'msg-cont';
+
+  		let msgName = document.createElement('span');
+  		msgName.className = 'msg-name';
+  		msgName.innerHTML = `${message[0]}`;
+
+  		let msgTime = document.createElement('span');
+  		msgTime.className = 'msg-time';
+  		msgTime.innerHTML = `${message[1]}`;
+
+  		let msgMsg = document.createElement('p');
+  		msgMsg.className = 'msg-msg';
+  		msgMsg.innerHTML = `${message[2]}`;
+
+  		msgPic.append(msgImg);
+  		msgCont.append(msgName);
+  		msgCont.append(msgTime);
+  		msgCont.append(msgMsg);
+
+  		msgInfo.append(msgPic);
+  		msgInfo.append(msgCont);
+  		msgContainer.append(msgInfo);
+
+
+  		document.querySelector('.msg-gutter').append(msgContainer);
+	}
+};
