74951a0a6bc4707e867e54f643cbabc308af186f
Emmanuel <emmanueltopea@gmail.com>
2020-10-05 14:25:34 +0100

    add new socket function to handle localstorage items

--- hook/routes/chat.py
+++ hook/routes/chat.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, session, request, jsonify
 from hook.routes.auth import login_required
 from flask_socketio import emit, join_room
 from .. import socketio
-from ..models import Channel, User
+from ..models import Channel, User, Message, db
 
 
 chat = Blueprint('chat', __name__)
@@ -25,31 +25,37 @@ def _process_info(name, n_type):
             new_channel.save()
             return new_channel
         else:
-        	return 'Error'
+        	return ('Error', 'Channel already exists!')
+    elif n_type == 'DM':
+    	pass
+    else:
+    	return None
 
 
 @chat.route('/add-new-obj', methods=['POST'])
-def add_new_channel():
-    name = request.form.get('name')
-    new_type = request.form.get('type')
-    if new_type == 'channel':
-        name = name.strip()
-        if name:
-            new_channel = _process_info(name, new_type)
-            if new_channel == 'Error':
-            	# return error if channel already exists
-                return jsonify({'success': False, 'error': 'Channel already exists!'})
+def add_new_obj():
+	name = request.form.get('name')
+	new_type = request.form.get('type')
+
+	if new_type == 'channel':
+		name = name.strip()
+		if name:
+			new_channel = _process_info(name, new_type)
+			# check foor error
+			if new_channel[0] == 'Error':
+				# return error if channel already exists
+				return jsonify({'success': False, 'error': new_channel[1]})
             # return success
-            return jsonify({'name': new_channel.channel_name, 'success': True,
+			return jsonify({'name': new_channel.channel_name, 'success': True,
                             'error': 'null'})
-        else:
-        	# return invalid input
-        	return jsonify({'success': False, 'error': 'Invalid input.'})
-    elif new_type == 'DM':
-        return jsonify({'name': 'ME', 'success': True,
-                       'error': 'null'})
-    else:
-        pass
+		else:
+			# return invalid input
+			return jsonify({'success': False, 'error': 'Invalid input.'})
+	elif new_type == 'DM':
+		return jsonify({'name': 'ME', 'success': True,
+			'error': 'null'})
+	else:
+		pass
 
 # ------------- SOCKETS CODES --------------
 
@@ -57,6 +63,7 @@ def add_new_channel():
 def connected(data):
 	""" confirm connection of sockets """
 	print(data['data'])
+	emit('setLocalStorage', {'name': session.get('user').username})
 
 
 @socketio.on('getChannelDetails', namespace='/chat')
@@ -68,7 +75,7 @@ def get_channel_details(data):
 	channel_name = data['name'][1:]  # excluding the '#' symbol
 	channel_id = data['id']
 
-	current_user = session.get('user').username # check for alignment of message in script
+	current_user = session.get('user').username
 	# confirm existence of channel
 	channel = Channel.query.get(channel_id)
 	# on existence get channel messages
@@ -84,15 +91,44 @@ def get_channel_details(data):
 			                 message.message])
 		try:
 			# enter channel as a room
-			room = join_room(channel_name)
 			join_room(channel_name)
+			room = join_room(channel_name)
 			# return details to script
 			emit('channelMessagesDelivered', {"messages": messages,
 			     "user": current_user}, room=room)
 		except Exception:
-			emit('ErrorJoiningChannel', {'error': 'Couldn\'t join channel'},
+			emit('Error', {'error': 'Couldn\'t join channel'},
 		          broadcast=False)
 
 	else:
-		emit('ChannelDoesNotExist', {'error': 'Channel does not exists'},
+		emit('Error', {'error': 'Channel does not exists'},
 			 broadcast=False)
+
+@socketio.on('sendMessageToChannel', namespace='/chat')
+def send_message_to_channel(data):
+	message = data['message']
+	channel_name = data['room'][1:]
+
+	# ensure valid message
+	if message:
+		try:
+			user = session.get('user')
+			channel = Channel.query.filter_by(channel_name=channel_name).one()
+			new_message = Message(message)
+			new_message.user_id = user.id
+			new_message.channel_id = channel.id
+			new_message.save()
+			# add user info
+			user_name = User.query.filter_by(id=user.id).one().username
+			time = new_message.timestamp
+			timestamp = ' ' + str(time.date()) + ' | ' + str(time.strftime('%H:%M'))
+			msg = new_message.message
+			message_info = [user_name, timestamp, msg]
+			room = channel_name
+			
+			emit('broadcastMessage', {'message': message_info}, room=room)
+		except Exception:
+			emit('Error', {'error': 'Error sending message.'})
+	else:
+		print("Pssd..")
+
