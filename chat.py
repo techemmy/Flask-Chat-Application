3c0f62f6b767cfd5754ec75e2873e70e7b287299
Emmanuel Temitope <emmanueltopea@gmail.com>
2020-12-21 16:41:07 +0100

    solve index.lock error

--- chat.py
+++ chat.py
@@ -1,6 +1,18 @@
 from hook import create_app, socketio
+from hook.models import User, Channel, Message, Dm, db
 
 app = create_app(debug=True)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Channel=Channel, Message=Message, Dm=Dm)
+
+
+@app.cli.command
+def discover_test():
+    pass
+
 
 if __name__ == '__main__':
     socketio.run(app)
