f1531f35c6d7e133da8b22e59821ff9fc10f74d3
Emmanuel Temitope <emmanueltopea@gmail.com>
2021-06-15 23:30:09 +0100

    add flask command to run test

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
+import os
+from dotenv import load_dotenv
 from hook import create_app, socketio
 from hook.models import User, Channel, Message, Dm, db
 
 app = create_app(debug=False)
 
+# load configurations from .env file
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 @app.shell_context_processor
 def make_shell_context():
@@ -10,7 +16,14 @@ def make_shell_context():
 
 @app.cli.command()
 def create_db():
+    db.create_all()
     print("DB CREATED...")
+
+@app.cli.command()
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 if __name__ == '__main__':
