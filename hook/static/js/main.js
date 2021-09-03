79438a3a50faede1522dbfe649900c6bf62442bd
Emmanuel Temitope <emmanueltopea@gmail.com>
2021-09-03 16:10:06 +0100

    fix sig-up button error - not working

--- hook/static/js/main.js
+++ hook/static/js/main.js
@@ -1,24 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    window.onload = () => {
-        $('#signup').modal('show');
-    }
-    var signUpBtn = document.querySelector('.signup');
-    if (signUpBtn){
-		signUpBtn.onclick = () => {
-       		$('#signup').modal('show');
-    	}; 
-    };
-    
-});
 
-let menuButton = document.querySelector(".menu__button");
-let menuWrap = document.querySelector(".menu-wrap");
+window.onload = () => {
+    $('#signup').modal('show');
+}
+var signUpBtn = document.querySelector('#signup_btn');
 
-menuButton.addEventListener("click", () => {
-    toggleMenu();
-});
-
-let toggleMenu = () => {
-    menuWrap.classList.toggle("open");
+if (signUpBtn){
+	signUpBtn.onclick = () => {
+   		$('#signup-modal').modal('show');
+	}; 
 };
-
