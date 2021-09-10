db2486a02c7157a9390b1c75e2e8d8fc509c5f30
Emmanuel Temitope <emmanueltopea@gmail.com>
2021-09-10 18:38:26 +0100

    configure login modal button

--- hook/static/js/main.js
+++ hook/static/js/main.js
@@ -2,10 +2,17 @@
 window.onload = () => {
     $('#signup').modal('show');
 }
-var signUpBtn = document.querySelector('#signup_btn');
+var signUpBtn = document.querySelector('#signup-btn');
+var loginBtn = document.querySelector('#login-btn')
 
 if (signUpBtn){
 	signUpBtn.onclick = () => {
    		$('#signup-modal').modal('show');
+	}; 
+};
+
+if (loginBtn){
+	loginBtn.onclick = () => {
+   		$('#login-modal').modal('show');
 	}; 
 };
